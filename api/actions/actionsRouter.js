e5eaa1a4655941a2cd2c1af5583a306e12f559b6
Michael Nunes <miketnunes@gmail.com>
2019-12-06 08:26:27 -0800

    Add empty routes for CRUD on actions

--- api/actions/actionsRouter.js
+++ api/actions/actionsRouter.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
 
+//GET /actions/:id - Returns all actions for a specified project.
+router.get('/:id', (req, res) => {
 
+});
 
+//POST /actions/:id - Creates a new action for a project.
+router.post('/:id', (req, res) => {
+
+});
+
+//PUT /actions/:id - Modifies an existing action by id.
+router.put('/:id', (req, res) => {
+
+});
+
+//DELETE /actions/:id - Deletes an existing action by id.
+router.delete("/:id", (req, res) => {
+
+});
 
 
 
