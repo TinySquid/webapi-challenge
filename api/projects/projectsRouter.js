bb1ca8b93c169d6a58405b9be8f2a16ddabc4505
Michael Nunes <miketnunes@gmail.com>
2019-12-06 09:04:23 -0800

    Finish CRUD for projects

--- api/projects/projectsRouter.js
+++ api/projects/projectsRouter.js
@@ -6,7 +6,7 @@ const projectDB = require('../../data/helpers/projectModel');
 router.get('/', (req, res) => {
   projectDB.get()
     .then(projects => {
-      if (projects) {
+      if (projects.length > 0) {
         res.status(200).json(projects);
       } else {
         res.status(404).json({ message: "No projects found" });
@@ -17,7 +17,7 @@ router.get('/', (req, res) => {
     });
 });
 
-//GET /projects/:id - Returns all projects.
+//GET /projects/:id - Returns project by id.
 router.get('/:id', (req, res) => {
   const id = req.params.id;
 
@@ -35,18 +35,55 @@ router.get('/:id', (req, res) => {
 });
 
 //POST /projects - Creates a new project.
-router.post('/', (req, res) => {
+router.post('/', validateProject, (req, res) => {
+  const { name, description, completed } = req.body;
 
+  projectDB.insert({
+    name: name,
+    description: description,
+    completed: completed || false
+  })
+    .then(project => {
+      res.status(201).json(project);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Could not add project to database", error: error });
+    });
 });
 
 //PUT /projects/:id - Modifies an existing project by id.
-router.put('/:id', (req, res) => {
+router.put('/:id', validateProjectId, validateProject, (req, res) => {
+  const id = req.params.id;
+  const { name, description, completed } = req.body;
 
+  projectDB.update(id, {
+    name: name,
+    description: description,
+    completed: completed || false
+  })
+    .then(project => {
+      res.status(200).json(project);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Could not modify project", error: error });
+    });
 });
 
 //DELETE /projects/:id - Deletes an existing project by id.
-router.delete("/:id", (req, res) => {
+router.delete("/:id", validateProjectId, (req, res) => {
+  const id = req.params.id;
 
+  projectDB.remove(id)
+    .then(state => {
+      if (state) {
+        res.status(200).json({ message: "Project deleted" });
+      } else {
+        res.status(500).json({ message: "yeet" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Could not delete project", error: error });
+    });
 });
 
 //MIDDLEWARE
