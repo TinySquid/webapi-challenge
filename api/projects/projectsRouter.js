a34927acf68d7eac6e5ddb5b443619401ffd518e
Michael Nunes <miketnunes@gmail.com>
2019-12-06 08:25:08 -0800

    Add empty routes for CRUD on projects

--- api/projects/projectsRouter.js
+++ api/projects/projectsRouter.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
 
+//GET /projects - Returns all projects.
+router.get('/', (req, res) => {
 
+});
 
+//POST /projects - Creates a new project.
+router.post('/', (req, res) => {
+
+});
+
+//PUT /projects/:id - Modifies an existing project by id.
+router.put('/:id', (req, res) => {
+
+});
+
+//DELETE /projects/:id - Deletes an existing project by id.
+router.delete("/:id", (req, res) => {
+
+});
 
 
 
